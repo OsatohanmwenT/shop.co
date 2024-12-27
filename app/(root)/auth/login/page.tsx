7861f0185c2432ad4a179a1f8c2f7007fb7e7013
OsatohanmwenT <osarenkhoeosato45@gmail.com>
2024-12-27 12:36:46 +0100

    debugging

--- app/(root)/auth/login/page.tsx
+++ app/(root)/auth/login/page.tsx
@@ -7,6 +7,7 @@ import {redirect} from "next/navigation";
 const Page = async () => {
     const session = await auth();
     if (session) {
+        console.log("session", session);
         redirect("/");
     }
 
