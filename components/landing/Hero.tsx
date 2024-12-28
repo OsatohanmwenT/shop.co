5b67549bf6b4b044f676424c812b4e3e7fedd455
OsatohanmwenT <osarenkhoeosato45@gmail.com>
2024-12-28 12:25:16 +0100

    responsive layout updated

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = async () => {
                 <Image height={100} width={100} src="/Vector.png"
                        className="absolute top-[30vh] max-lg:w-[80px] max-lg:h-[80px] right-3 lg:top-10 md:right-[15vw] lg:right-5 xl:right-14"
                        alt="star" />
-                <Image height={60} width={60} src="/Vector.png" className="absolute top-[35vh] md:top-[450px] max-md:left-[4] max-lg:md:left-[20vw] lg:right-[38vw]" alt="star" />
+                <Image height={60} width={60} src="/Vector.png" className="absolute top-[35vh] md:top-[450px] max-md:left-[1] max-lg:md:left-[20vw] lg:right-[38vw]" alt="star" />
                 <div className="xl:size-[500px] flex items-center justify-center">
                     <img src={banner.image.asset.url} alt="banner image" />
                 </div>
