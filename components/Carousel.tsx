59112b71bc851f68718a4947d016a5ee08790174
OsatohanmwenT <osarenkhoeosato45@gmail.com>
2025-01-01 11:01:58 +0100

    carousel added

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,15 +1,29 @@
-import React from 'react'
+"use client"
+import React, {useState} from 'react'
+import {ArrowLeft, ArrowRight} from "lucide-react";
 
 interface Props {
     images: any[] | undefined
 }
 
 const Carousel = ({images}: Props) => {
+    const [imageIndex, setImageIndex] = useState(0);
+
+    const handleNext = () => {
+        if (!images) return;
+        setImageIndex(prev => prev < images.length - 1 ? prev + 1 : 0);
+    }
+
+    const handlePrev = () => {
+        if (!images) return;
+        setImageIndex((prev) => prev === 0 ? images.length - 1 : prev - 1);
+    }
+
     return (
-        <>
-            <div className="flex max-lg:order-2 lg:flex-col max-lg:items-center gap-2">
+        <div className="flex flex-col gap-6">
+            <div className="flex max-lg:order-2 max-lg:mb-5 lg:flex-col max-lg:items-center max-lg:justify-center gap-2">
                 {images && images.map((image, i) => (
-                    <div key={image._key}
+                    <div onMouseEnter={() => setImageIndex(i)} key={i}
                          className="rounded-xl w-[80px] aspect-square overflow-hidden hover:border-blue-500 border-2">
                         <picture className="w-full h-full">
                             <img className="object-cover object-center" src={image?.asset?.url} alt={image._key}/>
@@ -17,12 +31,20 @@ const Carousel = ({images}: Props) => {
                     </div>
                 ))}
             </div>
-            <div className="rounded-xl max-lg:order-1 overflow-hidden">
-                <picture className="h-full w-full">
-                    <img className="object-center" src="" alt=""/>
-                </picture>
+            <div className="relative h-[400px] max-lg:order-1">
+                <button onClick={handlePrev} className="absolute top-[50%] -translate-y-[50%] lg:-left-16 bg-white rounded-full p-4">
+                    <ArrowLeft />
+                </button>
+                <div className="h-[400px] flex justify-center lg:w-[400px] rounded-xl overflow-hidden">
+                    <picture className="lg:mx-auto">
+                        <img className="object-center w-[200px] lg:w-full h-full object-contain" src={images && images[imageIndex]?.asset?.url} alt=""/>
+                    </picture>
+                </div>
+                <button onClick={handleNext} className="absolute rounded-full right-0 bg-white p-4 top-[50%] -translate-y-[50%] lg:-right-16">
+                    <ArrowRight />
+                </button>
             </div>
-        </>
+        </div>
     )
 }
 export default Carousel
