496ab7da98eadf945f82ae335f067e95661c176d
OsatohanmwenT <osarenkhoeosato45@gmail.com>
2025-01-06 11:59:44 +0100

    filtering updated

--- stores/filterStore.ts
+++ stores/filterStore.ts
@@ -5,7 +5,7 @@ import {create} from 'zustand';
 interface FilterState {
     filters: Record<string, string | string[]>; // Can be a string or array
     setFilter: (name: string, value: string | string[]) => void;
-    initializeFromURL: (urlParams: URLSearchParams) => void;
+    initializeFromURL: (urlParams: Record<string, string | string[]>) => void;
 }
 
 export const useFilterStore = create<FilterState>((set) => ({
