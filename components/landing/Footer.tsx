ad45078a60750c395642f04ecf7e5de672569fb3
OsatohanmwenT <osarenkhoeosato45@gmail.com>
2025-01-07 02:30:18 +0100

    footer updated

--- components/landing/Footer.tsx
+++ components/landing/Footer.tsx
@@ -14,9 +14,9 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <div className="py-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 bg-black text-white font-work-sans text-sm mt-24">
+        <footer className="py-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 bg-black text-white font-work-sans text-sm mt-24">
             {/* TOP */}
-            <div className="flex flex-col md:flex-row justify-between gap-24">
+            <div className="flex flex-col max-sm:items-center max-sm:text-center md:flex-row justify-between gap-24">
                 {/* LEFT */}
                 <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
                     <Link href="/">
@@ -95,7 +95,7 @@ const Footer = () => {
                 </div>
             </div>
             {/* BOTTOM */}
-            <div className="flex flex-col md:flex-row items-center justify-between gap-8 mt-16">
+            <div className="flex flex-col md:flex-row max-sm:text-center items-center justify-between gap-8 mt-16">
                 <div className="">&copy; SHOP.CO 2024. All Rights Reserved.</div>
                 <div className="flex flex-col gap-8 md:flex-row">
                     <div className="">
@@ -108,7 +108,7 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </footer>
     );
 };
 
